71f875e0c0ef4fbf38989591e3b1e07845c7a937
André Felipe <andre.felipe.2048@gmail.com>
2023-08-01 19:04:00 -0300

    feat: implement component AmountBox rendering data from an API

--- src/app/components/amount-box/amount-box.component.ts
+++ src/app/components/amount-box/amount-box.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AmountDataModel } from 'src/app/model/amountDataModel';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-amount-box',
   templateUrl: './amount-box.component.html',
   styleUrls: ['./amount-box.component.css']
 })
-export class AmountBoxComponent {
+export class AmountBoxComponent implements OnInit {
+  accountDetails: AmountDataModel = {
+    amountValue: 0,
+    limit: 0,
+    totalAmount: 0
+  }
+
+  constructor(private cardsService: CardsService) {}
+
+  ngOnInit(): void {
+    this.getAmountValues()
+  }
+
+  getAmountValues() {
+    this.cardsService.getCard().subscribe(data => {
+      this.accountDetails.amountValue = data.account.balance
+      this.accountDetails.limit = data.account.limit
+      this.accountDetails.totalAmount = this.accountDetails.amountValue + this.accountDetails.limit
+    })
+  }
 
 }
