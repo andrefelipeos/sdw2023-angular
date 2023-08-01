8f280b40f1d2e4ef9cb3dedc82371a543e8d3c4a
André Felipe <andre.felipe.2048@gmail.com>
2023-08-01 14:52:30 -0300

    feat: render card data from API using data biding

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountDataModel } from 'src/app/model/accountDataModel';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
   styleUrls: ['./card-box.component.css']
 })
-export class CardBoxComponent {
+export class CardBoxComponent implements OnInit {
   accountData: AccountDataModel = {
     name: "Darth Vader",
     account: {
@@ -18,4 +19,21 @@ export class CardBoxComponent {
       number: "8765 1234 1020 5070"
     }
   }
+
+  constructor(private cardsService: CardsService) {}
+
+  ngOnInit(): void {
+    this.getAccountData()
+  }
+
+  getAccountData() {
+    this.cardsService.getCard().subscribe(data => {
+      this.accountData.name = data.name
+      this.accountData.account.agency = data.account.agency
+      this.accountData.account.number = data.account.number
+      this.accountData.card.limit = data.card.limit
+      this.accountData.card.number = data.card.number.split("")[2]
+    })
+  }
+
 }
