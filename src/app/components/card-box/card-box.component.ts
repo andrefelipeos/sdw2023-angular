73ff46def0d003d584a64d713fbc582a1767c976
André Felipe <andre.felipe.2048@gmail.com>
2023-08-01 09:18:22 -0300

    temp: render card data using a model and data biding

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/accountDataModel';
 
 @Component({
   selector: 'app-card-box',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-box.component.css']
 })
 export class CardBoxComponent {
-
+  accountData: AccountDataModel = {
+    name: "Darth Vader",
+    account: {
+      agency: "1020",
+      number: "007654-12"
+    },
+    card: {
+      limit: 10000,
+      number: "8765 1234 1020 5070"
+    }
+  }
 }
